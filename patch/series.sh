918939deaf73460ae7daa1b0345ffa1dc6edd480
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2025-04-10 03:00:19 +0000

    New patches for abnormal edges
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/series.sh
+++ patch/series.sh
@@ -4,4 +4,5 @@ set -e
 
 git am --abort || true
 
-git am ../patch/skip_abnormal_edge.patch
+git am ../patch/0001-RISC-V-Do-not-lift-up-vsetvl-if-its-VL-is-used-PR119.patch
+git am ../patch/0001-RISC-V-vsetvl-elide-abnormal-edges-from-LCM-computat.patch
